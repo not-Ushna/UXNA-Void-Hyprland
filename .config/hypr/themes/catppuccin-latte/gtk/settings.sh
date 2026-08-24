d420afd22ba7825ee7c1a22367f0cd741d53b9ad
not-Ushna <ushnamondal@gmail.com>
2026-08-25 01:09:41 +0530

    sync latest configs and theme updates: 2026-08-25 01:09

---
+++ .config/hypr/themes/catppuccin-latte/gtk/settings.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+GTK_THEME="Adwaita"
+ICON_THEME="Papirus-Dark"
+CURSOR_THEME="Bibata-Modern-Classic"
+FONT="JetBrains Mono Nerd Font Book 10"
+
+gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
+gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
+gsettings set org.gnome.desktop.interface cursor-theme "$CURSOR_THEME"
+gsettings set org.gnome.desktop.interface font-name "$FONT"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+
+# Apply for hyprcursor if configured
+hyprctl setcursor "$CURSOR_THEME" 24
